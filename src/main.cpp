3ae52f03249857b95b5a85a75e25e9bf44abbb27
jdowe11 <jayrockd04@gmail.com>
2024-12-21 16:10:12 -0500

    Separated main function inside of iwcpp.cpp into its own file

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "iwcpp.hpp"
+
+int main(int argc, char* argv[]){ /// main program, will run the code
+    if(argc < 2){
+        cout << RED << "Error: " << RESET << "No input files" << endl;
+        exit(0);
+    }
+    IWCPP code(argv[1]);
+    code.run();
+    return 1;
+}
